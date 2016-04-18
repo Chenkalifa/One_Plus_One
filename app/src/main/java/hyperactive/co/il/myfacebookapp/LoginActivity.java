9603532994c0b4035b43b5d9d6b28515c8284d6a
chen kalifa <chenkalifa@gmail.com>
2016-04-18 23:00:23 +0300

    Initial commit

---
+++ app/src/main/java/hyperactive/co/il/myfacebookapp/LoginActivity.java
@@ -0,0 +1,278 @@
+package hyperactive.co.il.myfacebookapp;
+
+import android.app.Dialog;
+import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.facebook.AccessToken;
+import com.facebook.AccessTokenTracker;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.HttpMethod;
+import com.facebook.Profile;
+import com.facebook.ProfileTracker;
+import com.facebook.appevents.AppEventsLogger;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
+
+
+public class LoginActivity extends AppCompatActivity {
+    private LoginButton loginButton;
+    private LoginManager loginManager;
+    private CallbackManager callbackManager;
+    private AccessTokenTracker accessTokenTracker;
+    private ProfileTracker profileTracker;
+    private Profile profile;
+    private ImageView user_img;
+    private String friends;
+    final private String MY_LOG = "myLog";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        setContentView(R.layout.activity_login);
+
+        AppEventsLogger.activateApp(this);
+        profileTracker = new ProfileTracker() {
+            @Override
+            protected void onCurrentProfileChanged(Profile oldProfile, Profile currentProfile) {
+
+            }
+        };
+        accessTokenTracker = new AccessTokenTracker() {
+            @Override
+            protected void onCurrentAccessTokenChanged(AccessToken oldAccessToken, AccessToken currentAccessToken) {
+
+            }
+        };
+        profileTracker.startTracking();
+        accessTokenTracker.startTracking();
+        loginManager = LoginManager.getInstance();
+        loginButton = (LoginButton) findViewById(R.id.login_button);
+        loginButton.setReadPermissions("user_friends");
+        callbackManager = CallbackManager.Factory.create();
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                Log.i(MY_LOG, "success");
+                AccessToken accessToken = loginResult.getAccessToken();
+                Profile profile = Profile.getCurrentProfile();
+                new UserFriendsRequestTask().execute(accessToken);
+//                new GraphRequest(
+//                        accessToken,
+//                        "/me/friends",
+//                        null,
+//                        HttpMethod.GET,
+//                        new GraphRequest.Callback() {
+//                            public void onCompleted(GraphResponse response) {
+//
+//                                try {
+//                                    JSONArray friendsList = response.getJSONObject().getJSONArray("data");
+//                                    friends=friendsList.toString();
+//                                    Log.i(MY_LOG, friends);
+//
+//                                } catch (JSONException e) {
+//                                    e.printStackTrace();
+//                                }
+//                            }
+//                        }
+//                ).executeAsync();
+//                Intent intent = new Intent(LoginActivity.this, ShareActivity.class);
+//                intent.putExtra("friendsList", friends);
+//                startActivity(intent);
+            }
+
+            @Override
+            public void onCancel() {
+                Log.i(MY_LOG, "cancel");
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+                Log.e(MY_LOG, "error", error);
+                Toast.makeText(LoginActivity.this, error.toString(), Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        profile = Profile.getCurrentProfile();
+        if (profile != null) {
+            openAlreadyLoggedDialog();
+        } else {
+
+            loginManager.logInWithPublishPermissions(
+                    this,
+                    Arrays.asList("publish_actions"));
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        profileTracker.stopTracking();
+        accessTokenTracker.stopTracking();
+    }
+
+    private void openAlreadyLoggedDialog() {
+        final Dialog dialog = new Dialog(this);
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        dialog.setCancelable(false);
+        dialog.setContentView(R.layout.login_dialog);
+        user_img = (ImageView) dialog.findViewById(R.id.user_img);
+        TextView dialogtxt = (TextView) dialog.findViewById(R.id.dialog_txt);
+        dialogtxt.setText(getString(R.string.dialog_msg) + profile.getFirstName()+"\n"+getString(R.string.cuntinue_msg));
+        Uri profilePictureUri = profile.getProfilePictureUri(80, 80);
+        new PictureLoaderTask().execute(profilePictureUri);
+        Button ok = (Button) dialog.findViewById(R.id.ok_button);
+        Button cancel = (Button) dialog.findViewById(R.id.cancel_button);
+        ok.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new UserFriendsRequestTask().execute(AccessToken.getCurrentAccessToken());
+//                Intent intent = new Intent(LoginActivity.this, ShareActivity.class);
+//                startActivity(intent);
+            }
+        });
+        cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loginManager.logOut();
+                dialog.dismiss();
+            }
+        });
+        dialog.show();
+    }
+
+    private class UserFriendsRequestTask extends AsyncTask<AccessToken, Void, JSONObject>{
+       private JSONObject jsonResponse;
+        @Override
+        protected JSONObject doInBackground(AccessToken... params) {
+            Log.i(MY_LOG, "get user friends");
+            GraphRequest request = GraphRequest.newMeRequest(
+                    AccessToken.getCurrentAccessToken(),
+                    new GraphRequest.GraphJSONObjectCallback() {
+                        @Override
+                        public void onCompleted(
+                                JSONObject object,
+                                GraphResponse response) {
+                            Log.i(MY_LOG, "jsonObject="+object.toString());
+                            jsonResponse=object;
+                        }
+                    });
+            Bundle parameters = new Bundle();
+            parameters.putString("fields", "friends");
+            request.setParameters(parameters);
+            request.executeAndWait();
+
+
+//            new GraphRequest(
+//                    AccessToken.getCurrentAccessToken(),
+//                    "/me/friends",
+//                    null,
+//                    HttpMethod.GET,
+//                    new GraphRequest.Callback() {
+//                        public void onCompleted(GraphResponse response) {
+//                            graphResponse=response;
+//                        }
+//                    }
+//            ).executeAndWait();
+            return jsonResponse;
+        }
+
+
+        @Override
+        protected void onPostExecute(JSONObject jsonObject) {
+            super.onPostExecute(jsonObject);
+            if (jsonObject!=null){
+                try {
+                    friends=jsonObject.getJSONObject("friends").toString();
+                } catch (JSONException e) {
+                    Log.e(MY_LOG, "JSON error", e);
+                }
+                Log.i(MY_LOG, "friends="+friends);
+                    Intent intent = new Intent(LoginActivity.this, ShareActivity.class);
+                    intent.putExtra("friendsList", friends);
+                    startActivity(intent);
+            }
+        }
+    }
+
+    private class PictureLoaderTask extends AsyncTask<Uri, Void, Bitmap> {
+        @Override
+        protected void onPostExecute(Bitmap bitmap) {
+            super.onPostExecute(bitmap);
+            if (bitmap != null)
+                user_img.setImageBitmap(bitmap);
+            else
+                Log.i(MY_LOG, "bitmap null");
+        }
+
+        @Override
+        protected Bitmap doInBackground(Uri... params) {
+            Bitmap bitmap = null;
+            try {
+                URI path = new URI(params[0].toString());
+                URL picUrl = path.toURL();
+                InputStream inputStream = (InputStream) picUrl.getContent();
+                bitmap = BitmapFactory.decodeStream(inputStream);
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (URISyntaxException e) {
+                e.printStackTrace();
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return bitmap;
+        }
+    }
+}
---
+++ app/src/main/java/hyperactive/co/il/myfacebookapp/ShareActivity.java
@@ -0,0 +1,295 @@
+package hyperactive.co.il.myfacebookapp;
+
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TableRow;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.HttpMethod;
+import com.facebook.Profile;
+import com.facebook.share.ShareApi;
+import com.facebook.share.Sharer;
+import com.facebook.share.model.SharePhoto;
+import com.facebook.share.model.SharePhotoContent;
+import com.facebook.share.widget.ShareButton;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+
+
+public class ShareActivity extends AppCompatActivity implements View.OnFocusChangeListener {
+    private Toolbar toolbar;
+    private Profile profile;
+    private TextView shareTv;
+    private TableRow inputRow;
+    private EditText pic1UrlEt, pic1TextEt, pic2UrlEt, pic2TextEt, pic3UrlEt, pic3TextEt;
+    private ImageView img1, img2, img3;
+    private Bitmap image;
+    final private String MY_LOG = "myLog";
+    private String friends;
+    private JSONObject friendsList;
+    private Drawable et_original_background;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_share);
+        toolbar = (Toolbar) findViewById(R.id.share_toolbar);
+        setSupportActionBar(toolbar);
+        getUserFriends();
+        inputRow = (TableRow) findViewById(R.id.input_row);
+        shareTv = (TextView) findViewById(R.id.toolbar_share_btn);
+        pic1UrlEt = (EditText) findViewById(R.id.img_1_url_et);
+        pic1TextEt = (EditText) findViewById(R.id.img_1_txt_et);
+        et_original_background = pic1TextEt.getBackground();
+        img1 = (ImageView) findViewById(R.id.img_1);
+        pic2UrlEt = (EditText) findViewById(R.id.img_2_url_et);
+        pic2TextEt = (EditText) findViewById(R.id.img_2_txt_et);
+        img2 = (ImageView) findViewById(R.id.img_2);
+        pic3UrlEt = (EditText) findViewById(R.id.img_3_url_et);
+        pic3TextEt = (EditText) findViewById(R.id.img_3_txt_et);
+        img3 = (ImageView) findViewById(R.id.img_3);
+        pic1UrlEt.setOnFocusChangeListener(this);
+        pic2UrlEt.setOnFocusChangeListener(this);
+        pic3UrlEt.setOnFocusChangeListener(this);
+        profile = Profile.getCurrentProfile();
+        shareTv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i("myLog", "going to post picture");
+                postPicture();
+            }
+        });
+    }
+
+    private void getUserFriends() {
+        Intent callingIntent=getIntent();
+        if (callingIntent!=null){
+            try {
+                friendsList=new JSONObject(callingIntent.getStringExtra("friendsList"));
+                Log.i(MY_LOG,friendsList.toString());
+            } catch (JSONException e) {
+                Log.e(MY_LOG, "JSON error", e);
+            }
+        }
+    }
+
+    private void postPicture() {
+        Log.i("myLog", "in post picture");
+        //save the screenshot
+        removeViews();
+        View rootView = findViewById(R.id.activity_share_layout);//findViewById(android.R.id.content).getRootView()
+        rootView.setDrawingCacheEnabled(true);
+        image = Bitmap.createBitmap(rootView.getDrawingCache());
+        rootView.destroyDrawingCache();
+
+        //share dialog
+        final Dialog dialog = new Dialog(this);
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        dialog.setCancelable(false);
+        dialog.setContentView(R.layout.share_dialog);
+        Button yesBtn= (Button) dialog.findViewById(R.id.dialog_share_yes_btn);
+        Button cancelBtn= (Button) dialog.findViewById(R.id.dialog_share_cancel_btn);
+        yesBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //share the image to Facebook
+                SharePhoto photo = new SharePhoto.Builder().setBitmap(image).build();
+                SharePhotoContent content = new SharePhotoContent.Builder().addPhoto(photo).build();
+                returnViews();
+                ShareApi.share(content, new FacebookCallback<Sharer.Result>() {
+                    @Override
+                    public void onSuccess(Sharer.Result result) {
+                        Log.i(MY_LOG, "share success");
+                        Toast.makeText(ShareActivity.this, R.string.share_success_msg, Toast.LENGTH_LONG).show();
+                    }
+
+                    @Override
+                    public void onCancel() {
+                        Log.i(MY_LOG, "share cancel");
+                        Toast.makeText(ShareActivity.this, R.string.share_cancel_msg, Toast.LENGTH_LONG).show();
+                    }
+
+                    @Override
+                    public void onError(FacebookException error) {
+                        Log.i(MY_LOG, "share error");
+                        Toast.makeText(ShareActivity.this, R.string.share_error_msg, Toast.LENGTH_LONG).show();
+                    }
+                });
+                dialog.dismiss();
+            }
+        });
+        cancelBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                returnViews();
+                dialog.cancel();
+            }
+        });
+        dialog.show();
+//        AlertDialog.Builder shareDialog = new AlertDialog.Builder(this);
+//
+//        shareDialog.setTitle("Share Screen Shot");
+//        shareDialog.setMessage("Share image to Facebook?");
+//        shareDialog.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+//            public void onClick(DialogInterface dialog, int which) {
+//                //share the image to Facebook
+//                SharePhoto photo = new SharePhoto.Builder().setBitmap(image).build();
+//                SharePhotoContent content = new SharePhotoContent.Builder().addPhoto(photo).build();
+//                returnViews();
+//                ShareApi.share(content, new FacebookCallback<Sharer.Result>() {
+//                    @Override
+//                    public void onSuccess(Sharer.Result result) {
+//                        Log.i(MY_LOG, "share success");
+//                        Toast.makeText(ShareActivity.this, R.string.share_success_msg, Toast.LENGTH_LONG).show();
+//                    }
+//
+//                    @Override
+//                    public void onCancel() {
+//                        Log.i(MY_LOG, "share cancel");
+//                        Toast.makeText(ShareActivity.this, R.string.share_cancel_msg, Toast.LENGTH_LONG).show();
+//                    }
+//
+//                    @Override
+//                    public void onError(FacebookException error) {
+//                        Log.i(MY_LOG, "share error");
+//                        Toast.makeText(ShareActivity.this, R.string.share_error_msg, Toast.LENGTH_LONG).show();
+//                    }
+//                });
+//            }
+//        });
+//        shareDialog.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+//            public void onClick(DialogInterface dialog, int which) {
+//                returnViews();
+//                dialog.cancel();
+//            }
+//        });
+//        shareDialog.show();
+    }
+
+    private void removeViews() {
+        pic1UrlEt.requestFocus();
+        toolbar.setVisibility(View.GONE);
+        inputRow.setVisibility(View.GONE);
+        checkTextTv(pic1TextEt);
+        checkTextTv(pic2TextEt);
+        checkTextTv(pic3TextEt);
+    }
+
+    private void checkTextTv(EditText textEt) {
+        if (textEt.getText().toString().equals(""))
+           textEt.setVisibility(View.INVISIBLE);
+        else
+            textEt.setBackgroundResource(R.drawable.no_shape);
+    }
+
+    private void returnViews() {
+        toolbar.setVisibility(View.VISIBLE);
+        inputRow.setVisibility(View.VISIBLE);
+        pic1TextEt.setVisibility(View.VISIBLE);
+        pic1TextEt.setBackgroundDrawable(et_original_background);
+        pic2TextEt.setVisibility(View.VISIBLE);
+        pic2TextEt.setBackgroundDrawable(et_original_background);
+        pic3TextEt.setVisibility(View.VISIBLE);
+        pic3TextEt.setBackgroundDrawable(et_original_background);
+    }
+
+    @Override
+    public void onFocusChange(View v, boolean hasFocus) {
+        View vv=v;
+        switch (vv.getId()) {
+            case R.id.img_1_url_et:
+                Log.i(MY_LOG, "in image1");
+                getPicture(pic1UrlEt, img1);
+                break;
+            case R.id.img_2_url_et:
+                Log.i(MY_LOG, "in image2");
+                getPicture(pic2UrlEt, img2);
+                break;
+            case R.id.img_3_url_et:
+                Log.i(MY_LOG, "in image3");
+                getPicture(pic3UrlEt, img3);
+                break;
+        }
+
+    }
+
+    private void getPicture(EditText picUrlEt, ImageView image) {
+        try {
+            new PictureLoaderTask(picUrlEt, image).execute(new URL(picUrlEt.getText().toString()));
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    private class PictureLoaderTask extends AsyncTask<URL, Void, Bitmap> {
+        private EditText et;
+        private ImageView image;
+        PictureLoaderTask(EditText et, ImageView image){
+            this.et=et;
+            this.image=image;
+        }
+        @Override
+        protected void onPostExecute(Bitmap bitmap) {
+            super.onPostExecute(bitmap);
+            if (bitmap != null) {
+//                image.setBackground(null);
+                image.setBackgroundColor(getResources().getColor(android.R.color.transparent));
+                image.setImageBitmap(bitmap);
+                et.setWidth(80);
+                et.setText("");
+            } else{
+                Log.i(MY_LOG, "bitmap null");
+                Toast.makeText(ShareActivity.this, "Sorry, not a picture", Toast.LENGTH_LONG).show();
+            }
+
+        }
+
+        @Override
+        protected Bitmap doInBackground(URL... params) {
+            Bitmap bitmap = null;
+            try {
+                InputStream inputStream = (InputStream) params[0].getContent();
+                bitmap = BitmapFactory.decodeStream(inputStream);
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return bitmap;
+        }
+    }
+}
